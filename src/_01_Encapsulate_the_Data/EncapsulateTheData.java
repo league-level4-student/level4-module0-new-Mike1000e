b059a4485c05c77e3e5828351b257676e0794a1f
Mike1000e <56172395+Mike1000e@users.noreply.github.com>
2022-01-21 18:27:28 -0800

    encapsulating and ENUMS

--- src/_01_Encapsulate_the_Data/EncapsulateTheData.java
+++ src/_01_Encapsulate_the_Data/EncapsulateTheData.java
@@ -7,7 +7,20 @@ public class EncapsulateTheData {
      * itemsReceived to 0.
      */
 
-    int itemsReceived;
+   private  int itemsReceived;
+    
+    public void setItemsReceived(int randomPositiveNum) {
+		this.itemsReceived = randomPositiveNum;
+		 if(itemsReceived < 0) {
+		    	itemsReceived = 0;
+		    }
+	}
+
+	public Integer getItemsReceived() {
+		// TODO Auto-generated method stub
+		
+		return itemsReceived;
+	}
 
     /*
      * degreesTurned must be locked between 0.0 and 360.0 inclusive. All
@@ -15,20 +28,74 @@ public class EncapsulateTheData {
      * bound.
      */
 
-    double degreesTurned;
+    private double degreesTurned;
+    public void setDegreesTurned(double randomPositiveNum) {
+		// TODO Auto-generated method stub
+		this.degreesTurned = randomPositiveNum;
+		if(degreesTurned<0.0) {
+			degreesTurned = 0.0;
+		}
+		if(degreesTurned>360.0) {
+			degreesTurned = 360.0;
+		}
+	}
 
+	public Double getDegreesTurned() {
+		// TODO Auto-generated method stub
+		
+		return degreesTurned;
+		
+	}
     /*
      * nomenclature must not contain an empty String. An empty String parameter
      * should set nomenclature to a String with a single space.
      */
 
-    String nomenclature;
-
+   private String nomenclature;
+   
+    public void setNomenclature(String randomString) {
+		// TODO Auto-generated method stub
+		this.nomenclature = randomString;
+		if(nomenclature.isEmpty()) {
+			nomenclature = " ";
+			
+		}
+		
+	}
+    
+    public Object getNomenclature() {
+		// TODO Auto-generated method stub
+		return nomenclature;
+	}
     /*
      * memberObj must not be a String. A String parameter should set memberObj
      * to a new Object(); Hint: Use the instanceof operator.
      */
 
-    Object memberObj;
+    private Object memberObj;
+
+	public void setMemberObj(Object randomObject) {
+		// TODO Auto-generated method stub
+		this.memberObj = randomObject;
+		if(memberObj instanceof String) {
+			memberObj = new Object();
+		}
+			
+		
+	}
+
+	public Object getMemberObj() {
+		// TODO Auto-generated method stub
+		
+		return memberObj;
+	}
+
+	
+
+	
+
+	
+
+	
 
 }
--- src/_04_Data_Type_Option_Pane/DataTypeOptionPane.java
+++ src/_04_Data_Type_Option_Pane/DataTypeOptionPane.java
@@ -6,5 +6,62 @@ public class DataTypeOptionPane {
     public static void main(String[] args) {
 
         DataType[] dataTypes;
+        dataTypes = DataType.values();
+        int chosen = JOptionPane.showOptionDialog(null, "Choose what you want to explore", null, 0, 0, null, dataTypes, dataTypes);
+        switch(dataTypes[chosen]) {
+        case BYTE : {
+        	JOptionPane.showMessageDialog(null, dataTypes[chosen].getBits());
+        	break;
+        }
+        case SHORT : {
+        	JOptionPane.showMessageDialog(null, dataTypes[chosen].getBits());
+        	break;
+        }
+        case INT : {
+        	JOptionPane.showMessageDialog(null, dataTypes[chosen].getBits());
+        	break;
+        }
+        case LONG  : {
+        	JOptionPane.showMessageDialog(null, dataTypes[chosen].getBits());
+        	break;
+        }
+        case FLOAT : {
+        	JOptionPane.showMessageDialog(null, dataTypes[chosen].getBits());
+        	break;
+        }
+        case DOUBLE : {
+        	JOptionPane.showMessageDialog(null, dataTypes[chosen].getBits());
+        	break;
+        }
+        case CHAR : {
+        	JOptionPane.showMessageDialog(null, dataTypes[chosen].getBits());
+        	break;
+        }
+        case BOOLEAN : {
+        	JOptionPane.showMessageDialog(null, dataTypes[chosen].getBits());
+        	break;
+        }
+        }
     }
+    enum DataType {
+    	BYTE(8), SHORT(16), INT(32), LONG(64), FLOAT(32), DOUBLE(64), BOOLEAN(1), CHAR(16);
+    	
+    	
+    	
+     private final int bits;
+    
+ 
+	private DataType(int bitz) {
+    this.bits=bitz;	 
+     }
+     public int getBits() {
+    	 return this.bits;
+    	 
+     }
+    	
+    
+    }
+
+
+	
 }
